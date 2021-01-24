2e7c4d97ff14e8883dd8f712b1a1cf5b8792a0dc
kindred <tgorham1980@gmail.com>
2021-01-23 23:26:21 -0500

    Added new while_loops2

---
+++ PyClass_New/while_loops2.py
@@ -0,0 +1,10 @@
+
+n = 1
+loop = 1
+
+while n < 5:
+    print("Not quite there yet, running loop ", loop)
+    loop = loop + 1
+    n = n + 1
+else:
+    print("Final loop condition is 'False' on loop ", loop)
